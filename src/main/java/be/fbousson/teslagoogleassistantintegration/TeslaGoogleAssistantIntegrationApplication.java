3af6ceeb2828aee2a250cfecc91ed4bcc7d5b814
Frederick Bousson <fbousson@gmail.com>
2022-12-11 16:58:08 +0100

    initial project setup

---
+++ src/main/java/be/fbousson/teslagoogleassistantintegration/TeslaGoogleAssistantIntegrationApplication.java
@@ -0,0 +1,13 @@
+package be.fbousson.teslagoogleassistantintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeslaGoogleAssistantIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeslaGoogleAssistantIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/fbousson/teslagoogleassistantintegration/TeslaGoogleAssistantIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package be.fbousson.teslagoogleassistantintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeslaGoogleAssistantIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
